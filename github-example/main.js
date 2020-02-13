47ef954f9511c00a4db71433c2df9a72c3511f6c
bomjin <47442949+bomjin@users.noreply.github.com>
2020-02-13 23:31:54 +0900

    02/13 Commit

--- github-example/main.js
+++ github-example/main.js
@@ -6,12 +6,24 @@
 
     StaggleBtn.addEventListener('click', function () {
         toggleElements();
+    });  // 클릭
+
+    window.addEventListener('resize',  function( ) {
+        if(window.innerWidth > 1024) {
+            offElements();
+        }
+
     });
 
     function toggleElements() {
         [].forEach.call($toggles, function(toggle){
             toggle.classList.toggle('on');
         });
+    }
+    function offElements() {
+        [].forEach.call($toggles, function(toggle){
+            toggle.classList.remove('on'); // 토글 삭제 
+        });  
     }
 })
 (Window, document)
